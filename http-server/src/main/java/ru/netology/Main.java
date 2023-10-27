826a9da294c35d45970c94d16b5b85c06d029afa
Kirill Makarov <maku87143@gmail.com>
2023-10-28 02:55:18 +0300

    Initial commit

---
+++ http-server/src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(9999);
+        server.openConnection();
+    }
+}
+
+
---
+++ http-server/src/main/java/ru/netology/Server.java
@@ -0,0 +1,97 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+public class Server {
+    private static final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
+            "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html",
+            "/events.html", "/events.js");
+    private final ExecutorService threadPool = newFixedThreadPool(64);
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void openConnection() {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final Socket socket = serverSocket.accept();
+                threadPool.submit(() -> handleConnection(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConnection(Socket socket) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            final String requestLine = in.readLine();
+            final String[] parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                return;
+            }
+
+            final String path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final Path filePath = Path.of(".", "public", path);
+            final String mimeType = Files.probeContentType(filePath);
+
+            if (path.equals("/classic.html")) {
+                final String template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + content.length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+            } else {
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
